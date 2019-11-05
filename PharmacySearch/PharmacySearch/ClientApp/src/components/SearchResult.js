dc1a5130a2077d630888b87ce3cd7a545e3e1d12
Michał Polak <235046@student.pwr.edu.pl>
2019-11-06 00:03:45 +0100

    implemented view and search on front

--- PharmacySearch/PharmacySearch/ClientApp/src/components/SearchResult.js
+++ PharmacySearch/PharmacySearch/ClientApp/src/components/SearchResult.js
@@ -1,8 +1,52 @@
 import { Input, Button } from 'antd';
 import axios from 'axios';
 import React from 'react';
+import {
+    Container,
+    ListGroup,
+    ListGroupItem,
+    Table,
+    Col,
+} from 'reactstrap';
+import 'antd/lib/table/style/css';
 
 const { Search } = Input;
+
+const columns = [
+    { title: 'Id', dataIndex: 'id', key: 'id' },
+    { title: 'Name', dataIndex: 'name', key: 'name' },
+    { title: 'FullName', dataIndex: 'fullName', key: 'fullName' },
+    { title: 'Type', dataIndex: 'type', key: 'type' },
+    { title: 'Capacity', dataIndex: 'capacity', key: 'capacity' },
+    { title: 'Prescription', dataIndex: 'prescription', key: 'prescription' },
+    {
+        dataIndex: '',
+        key: 'FullName',
+    },
+];
+
+// const Row = ({ name, fullName, type, capacity, prescription, id }) =>
+//     [
+//         { title: 'Id', dataIndex: 'id', key: 'id' },
+//         { title: 'Name', dataIndex: 'name', key: 'name' },
+//         { title: 'FullName', dataIndex: 'fullName', key: 'fullName' },
+//         { title: 'Type', dataIndex: 'type', key: 'type' },
+//         { title: 'Capacity', dataIndex: 'capacity', key: 'capacity' },
+//         { title: 'Prescription', dataIndex: 'prescription', key: 'prescription' },
+//         {
+//             dataIndex: '',
+//             key: 'id'
+//         },
+//     ];
+
+const Row = ({ medicineDtoName, medicineDtoFullName, medicineDtoType, medicineDtoCapacity, prescription, id }) =>
+    <tr>
+        <td>{medicineDtoName}</td>
+        <td>{medicineDtoFullName}</td>
+        <td>{medicineDtoType}</td><br />
+        <td>{medicineDtoCapacity}</td>
+        <td>{prescription}</td>
+    </tr>
 
 export default class SearchPage extends React.Component {
     constructor(props) {
@@ -62,10 +106,31 @@ export default class SearchPage extends React.Component {
                     name="text"
                     placeholder="Search.."
                     ref={node => this.searchValue = node}
-                    //onChange={this.onChange}
+                    onChange={this.onChange}
                     style={{ width: "300px" }}
                 />
                 <Button type="primary" shape="circle" icon="search" onClick={this.onChange} />
+
+                {/* <Table
+                    columns={columns}
+                    expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>}
+                    dataSource={result.map(Row)}
+                    rowKey={record => record.key}
+                /> */}
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>FullName</th>
+                            <th>Type</th><br />
+                            <th>Capacity</th>
+                            <th>Prescription</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {result.map(Row)}
+                    </tbody>
+                </Table>
             </div>
         )
     }
