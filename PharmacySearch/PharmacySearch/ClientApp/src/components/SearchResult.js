0f4999a1f9ed402fb29cb1f4da4b3e35ce654f89
Michał Polak <235046@student.pwr.edu.pl>
2019-11-12 13:02:59 +0100

    added redirect and changed main page

--- PharmacySearch/PharmacySearch/ClientApp/src/components/SearchResult.js
+++ PharmacySearch/PharmacySearch/ClientApp/src/components/SearchResult.js
@@ -1,11 +1,9 @@
-import { Input, Descriptions, Table } from 'antd';
+import { Descriptions, Table } from 'antd';
 import axios from 'axios';
 import React from 'react';
 import 'antd/lib/table/style/css';
 import 'antd/lib/input/style/css';
 import 'antd/lib/descriptions/style/css';
-
-const { Search } = Input;
 
 const columnsPharmacies = [
     { title: 'Name', dataIndex: 'name', key: 'name' },
@@ -18,51 +16,31 @@ export default class SearchPage extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            searchValue: '',
             medicine: [],
             pharmacies: []
         }
-        this.handleChangeSearchValue = this.handleChangeSearchValue.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
     };
 
-    handleChangeSearchValue = async (event) => {
-        const { target } = event;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const { name } = target;
-        await this.setState({
-            [name]: value,
-        });
-    };
-
-    onChange = e => {
-        const { value } = e.target;
-        this.setState({
-            searchValue: value
-        });
-    }
-
-    handleSearch = searchValue => {
-        const requestOptions =
-            axios.get('https://localhost:44399/api/medicine/' + this.state.searchValue,
-                {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .then((response) => {
-                    console.log("response", response.data[0].pharmaciesDto);
-                    const medicine = response.data;
-                    const pharmacies = response.data[0].pharmaciesDto;
-                    this.setState({ medicine, pharmacies });
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+    componentDidMount() {
+        axios.get('https://localhost:44399/api/medicine/' + localStorage.getItem('searchValue'),
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) => {
+                console.log("response", response.data[0].pharmaciesDto);
+                const medicine = response.data;
+                const pharmacies = response.data[0].pharmaciesDto;
+                this.setState({ medicine, pharmacies });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     render() {
-        const { medicine, pharmacies } = this.state;
+        const { medicine, pharmacies } = this.state
         const pharmacyData = pharmacies.map(p => ({
             name: p.pharmacyDtoName,
             address: p.pharmacyDtoAddress,
@@ -72,19 +50,9 @@ export default class SearchPage extends React.Component {
         }));
         return (
             <div>
-                <Search
-                    placeholder="input search text"
-                    enterButton="Search"
-                    size="large"
-                    onChange={(e) => {
-                        this.onChange(e)
-                    }}
-                    onSearch={this.handleSearch}
-                />
-                <Descriptions>
+                <Descriptions title="Medicine Info">
                     {medicine.map(row => (
                         <Descriptions.Item label="Name">{row.medicineDtoName}</Descriptions.Item>
-
                     ))}
                     {medicine.map(row => (
                         <Descriptions.Item label="Full Name">{row.medicineDtoFullName}</Descriptions.Item>
@@ -104,7 +72,7 @@ export default class SearchPage extends React.Component {
                     expandedRowRender={record => <p style={{ margin: 0 }}>{record.address}</p>}
                     dataSource={pharmacyData}
                 />
-            </div>
+            </div >
         )
     }
 }
